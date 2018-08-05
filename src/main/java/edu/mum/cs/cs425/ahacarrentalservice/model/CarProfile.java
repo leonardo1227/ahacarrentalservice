0e62f95493149172f3c04a671018a04ca14a8c4e
Son Nguyen <ngtson@gmail.com>
2018-08-05 17:27:28 -0500

    Implement for UC - Register Car Profile

---
+++ src/main/java/edu/mum/cs/cs425/ahacarrentalservice/model/CarProfile.java
@@ -0,0 +1,137 @@
+package edu.mum.cs.cs425.ahacarrentalservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class CarProfile {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String model;
+    private String brand;
+    private String color;
+    private Integer year;
+    private CarStatus status = CarStatus.PENDING;
+
+    public CarProfile() {
+    }
+
+    public CarProfile(String model, String brand, String color, Integer year, CarStatus status) {
+        this.model = model;
+        this.brand = brand;
+        this.color = color;
+        this.year = year;
+        this.status= status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+	public CarStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(CarStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((brand == null) ? 0 : brand.hashCode());
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((model == null) ? 0 : model.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((year == null) ? 0 : year.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CarProfile other = (CarProfile) obj;
+		if (brand == null) {
+			if (other.brand != null)
+				return false;
+		} else if (!brand.equals(other.brand))
+			return false;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (model == null) {
+			if (other.model != null)
+				return false;
+		} else if (!model.equals(other.model))
+			return false;
+		if (status != other.status)
+			return false;
+		if (year == null) {
+			if (other.year != null)
+				return false;
+		} else if (!year.equals(other.year))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CarProfile [id=" + id + ", model=" + model + ", brand=" + brand + ", color=" + color + ", year=" + year
+				+ ", status=" + status + "]";
+	}
+}
---
+++ src/main/java/edu/mum/cs/cs425/ahacarrentalservice/service/CarProfileService.java
@@ -0,0 +1,44 @@
+package edu.mum.cs.cs425.ahacarrentalservice.service;
+
+import edu.mum.cs.cs425.ahacarrentalservice.model.CarProfile;
+import edu.mum.cs.cs425.ahacarrentalservice.repository.ICarProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarProfileService implements ICarProfileService<CarProfile> {
+    @Autowired
+    private ICarProfileRepository repository;
+
+    public List<CarProfile> findAll(){
+        return repository.findAll();
+    }
+
+    @Override
+    public List<CarProfile> findAll(String orderingProperty) {
+        return repository.findAll(new Sort(Sort.Direction.ASC, orderingProperty));
+    }
+
+    @Override
+    public CarProfile findById(Long id) {
+        Optional<CarProfile> car = repository.findById(id);
+        return car.orElse(null);
+    }
+
+    @Override
+    public CarProfile save(CarProfile car) {
+        return repository.save(car);
+    }
+
+
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+
+}
